ea55d5d9f8a5f63a6e56098f5d47821d76f8c910
Anssi Laurento <anssi.laurento@gmail.com>
2020-02-06 14:11:59 +0200

    updated calenderController to use new Calendar class

--- src/main/java/com/mycompany/projektikalenteri/CalendarController.java
+++ src/main/java/com/mycompany/projektikalenteri/CalendarController.java
@@ -25,66 +25,62 @@ import javafx.scene.input.MouseEvent;
 public class CalendarController {
 
 	private Kayttaja kayttaja;
-	private Kalenteri kalenteri;
+	private Calendar calendar;
 	@FXML
 	private ResourceBundle resources;
 	@FXML
-    private GridPane monthPane;
-
-    @FXML
-    private Text monthName;
-    @FXML
-    private Text infoText;
-    @FXML
-    private ListView ownProjects;
-    @FXML
-    private ListView otherProjects;
-    private List<Projekti> list;
-
-	private void fillMonthPane() throws IOException {
-    	monthName.setText(kalenteri.getMonthName()+"   "+kalenteri.getYear());
-    	monthPane.getChildren().clear();
-    	monthPane.setGridLinesVisible(true);
-    	int day = 1;
-    	int firstDay = kalenteri.getFirstDayOfMonth();
-    	for (int i = 0; i <= 5; i++) {
-    		for (int j = 0; j < 7; j++){
-    			if (i == 0 && j==0) j = (j+firstDay);
-	    		if (day > kalenteri.getMaxDaysInMonth()) return;
-		    	Text text = new Text(Integer.toString(day));
-		    	day++;
-
-		    	monthPane.setColumnIndex(text, j);
-		    	monthPane.setRowIndex(text, i);
-		    	monthPane.getChildren().addAll(text);
-
-	    		}
-    	}
-
-    }
-
-    @FXML
-    private void handlePreviousMonthButton(ActionEvent event) throws IOException {
-    	kalenteri.setMonthToPrevious();
-    	fillMonthPane();
-    }
-    @FXML
-    private void handleNextMonthButton(ActionEvent event) throws IOException {
-    	kalenteri.setMonthToNext();
-    	fillMonthPane();
-    }
-
-
-
-
-
-
+	private GridPane monthPane;
 
 	@FXML
-    private void addEntry(ActionEvent event) {
-    	EntryScreenController c = new EntryScreenController();
-    	final Stage dialog = new Stage();
+	private Text monthName;
+	@FXML
+	private Text infoText;
+	@FXML
+	private ListView ownProjects;
+	@FXML
+	private ListView otherProjects;
+	private List<Projekti> list;
 
+	private void fillMonthPane() throws IOException {
+		monthName.setText(this.calendar.getMonthName() + "   " + this.calendar.getYear());
+		monthPane.getChildren().clear();
+		monthPane.setGridLinesVisible(true);
+		int day = 1;
+		int firstDay = this.calendar.getFirstDayOfMonth();
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j < 7; j++) {
+				if (i == 0 && j == 0)
+					j = (j + firstDay);
+				if (day > this.calendar.getMaxDaysInMonth())
+					return;
+				Text text = new Text(Integer.toString(day));
+				day++;
+
+				monthPane.setColumnIndex(text, j);
+				monthPane.setRowIndex(text, i);
+				monthPane.getChildren().addAll(text);
+
+			}
+		}
+
+	}
+
+	@FXML
+	private void handlePreviousMonthButton(ActionEvent event) throws IOException {
+		this.calendar.setMonthToPrevious();
+		fillMonthPane();
+	}
+
+	@FXML
+	private void handleNextMonthButton(ActionEvent event) throws IOException {
+		this.calendar.setMonthToNext();
+		fillMonthPane();
+	}
+
+	@FXML
+	private void addEntry(ActionEvent event) {
+		EntryScreenController c = new EntryScreenController();
+		final Stage dialog = new Stage();
 
 		try {
 
@@ -92,62 +88,62 @@ public class CalendarController {
 			AnchorPane pane = loader.load();
 			EntryScreenController controller = loader.getController();
 
-	        Scene scene = new Scene(pane);
+			Scene scene = new Scene(pane);
 
-	        dialog.setTitle(resources.getString("addEntryTitle"));
+			dialog.setTitle(resources.getString("addEntryTitle"));
 
-	        dialog.setScene(scene);
+			dialog.setScene(scene);
 
-	        dialog.show();
-	        controller.setKayttaja(kayttaja);
+			dialog.show();
+			controller.setKayttaja(kayttaja);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-    }
-    @FXML
-    private void addProject(ActionEvent event) {
-    	final Stage dialog = new Stage();
-        Parent root;
+	}
+
+	@FXML
+	private void addProject(ActionEvent event) {
+		final Stage dialog = new Stage();
+		Parent root;
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CreateProjectScene.fxml"), resources); //$NON-NLS-1$
-	        GridPane pane = loader.load();
-	        AddProjectController controller = loader.getController();
+			GridPane pane = loader.load();
+			AddProjectController controller = loader.getController();
 
-	        Scene scene = new Scene(pane);
+			Scene scene = new Scene(pane);
 
-	        dialog.setTitle(resources.getString("addProjectTitle"));
+			dialog.setTitle(resources.getString("addProjectTitle"));
 
-	        dialog.setScene(scene);
+			dialog.setScene(scene);
 
-	        dialog.show();
-	        controller.setAll(kayttaja, this);
-
+			dialog.show();
+			controller.setAll(kayttaja, this);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-    }
-    @FXML
+	}
+
+	@FXML
 	public void initialize() {
 
-    	kalenteri = new Kalenteri(resources);
-    	
-    	try {
+		this.calendar = new Calendar(resources);
+
+		try {
 			fillMonthPane();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
 
-
-    }
-    public Kayttaja getKayttaja() {
+	public Kayttaja getKayttaja() {
 		return kayttaja;
 
-    }
+	}
 
 	public void setKayttaja(Kayttaja kayttaja) {
 		this.kayttaja = kayttaja;
@@ -155,29 +151,26 @@ public class CalendarController {
 		fillInfo();
 
 	}
+
 	public void fillInfo() {
-		String text = resources.getString("user")+": "+kayttaja.getKayttajatunnus();
+		String text = resources.getString("user") + ": " + kayttaja.getKayttajatunnus();
 
 		infoText.setText(text);
 		list = kayttaja.getPomona();
 		List<Projekti> list2 = kayttaja.getTekijana();
 		List<String> list3 = new ArrayList();
 		List<String> list4 = new ArrayList();
-		for (Projekti p: list) {
+		for (Projekti p : list) {
 			list3.add(p.getNimi());
 
 		}
-		for (Projekti p: list2) {
+		for (Projekti p : list2) {
 			list4.add(p.getNimi());
 		}
 		ObservableList<String> items = FXCollections.observableArrayList(list3);
 		ObservableList<String> items2 = FXCollections.observableArrayList(list4);
 
-
-
 		ownProjects.setItems(items);
-
-
 
 		if (!items2.isEmpty()) {
 			otherProjects.setItems(items);
@@ -185,44 +178,40 @@ public class CalendarController {
 		}
 
 	}
+
 	@FXML
-    public void projectSettings(MouseEvent click) {
-		if(click.getClickCount() == 2 && !list.isEmpty()) {
+	public void projectSettings(MouseEvent click) {
+		if (click.getClickCount() == 2 && !list.isEmpty()) {
 			final Stage dialog = new Stage();
-	        Parent root;
+			Parent root;
 			try {
 				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/EditProjectScene.fxml"), resources);
 				AnchorPane pane = loader.load();
 
 				EditProjectController controller = loader.getController();
-		        Scene scene = new Scene(pane);
+				Scene scene = new Scene(pane);
 
-		        dialog.setTitle(resources.getString("addProjectTitle"));
+				dialog.setTitle(resources.getString("addProjectTitle"));
 
-		        dialog.setScene(scene);
+				dialog.setScene(scene);
 
-		        dialog.show();
-		        Projekti p = null;
-		        String s = ownProjects.getSelectionModel().getSelectedItem().toString();
-		        for (Projekti pr : list) {
-		        	if (pr.getNimi().equals(s)) {
-		        		p = pr;
-		        		break;
-		        	}
-		        }
-		        controller.setAll(kayttaja, p, this);
+				dialog.show();
+				Projekti p = null;
+				String s = ownProjects.getSelectionModel().getSelectedItem().toString();
+				for (Projekti pr : list) {
+					if (pr.getNimi().equals(s)) {
+						p = pr;
+						break;
+					}
+				}
+				controller.setAll(kayttaja, p, this);
 
-		        controller.setItems();
+				controller.setItems();
 
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-
-
-
-
-
 
 }
