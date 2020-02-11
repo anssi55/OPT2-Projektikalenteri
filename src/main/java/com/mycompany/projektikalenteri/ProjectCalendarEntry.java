23a7fc73543b8496c96ea77f07eee67ae3cf12b2
Anssi Laurento <anssi.laurento@gmail.com>
2020-02-11 15:17:35 +0200

    refactored calendarEntry class into 2 classes

---
+++ src/main/java/com/mycompany/projektikalenteri/ProjectCalendarEntry.java
@@ -0,0 +1,22 @@
+package main.java.com.mycompany.projektikalenteri;
+
+import java.time.LocalDate;
+
+import com.mycompany.projektikalenteri.CalendarEntry;
+
+public class ProjectCalendarEntry extends CalendarEntry {
+    private User user;
+
+    public ProjectCalendarEntry(int id, String entry, LocalDate startTime, LocalDate endTime, User user) {
+        super(id, entry, startTime, endTime);
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
